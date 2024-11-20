f09d6c4b6cd6bbbb6b81c65bd1f412e988bc6e13
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2024-11-20 12:16:48 -0600

    update: shader utils

--- src/Utils.py
+++ src/Utils.py
@@ -2,6 +2,8 @@ import pygame
 
 from OpenGL.GL import *
 
+from colorama import Fore, Style
+
 
 def drawText(position, text):
     font = pygame.font.Font(None, 20)
@@ -10,3 +12,18 @@ def drawText(position, text):
     glWindowPos3d(*position)
     glDrawPixels(textSurface.get_width(), textSurface.get_height(),
                  GL_RGBA, GL_UNSIGNED_BYTE, textData)
+
+def load_shader_from_file(path):
+    with open(path, 'r') as file:
+        return file.read()
+    
+def compile_shader(source, shader_type):
+    shader = glCreateShader(shader_type)
+    glShaderSource(shader, source)
+    glCompileShader(shader)
+    
+    if not glGetShaderiv(shader, GL_COMPILE_STATUS):
+        error = glGetShaderInfoLog(shader).decode()
+        raise RuntimeError(f"{Fore.RED}Shader compile error: {error}{Style.RESET_ALL}")
+    
+    return shader
