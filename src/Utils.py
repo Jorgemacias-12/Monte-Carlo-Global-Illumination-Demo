3c8d2c3a42fff5b5c30f07a53aea9681e6c9c291
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2024-11-26 02:52:29 -0600

    update: util function needed by the new code

--- src/Utils.py
+++ src/Utils.py
@@ -13,20 +13,39 @@ def drawText(position, text):
     glDrawPixels(textSurface.get_width(), textSurface.get_height(),
                  GL_RGBA, GL_UNSIGNED_BYTE, textData)
 
+
 def load_shader_from_file(path):
     with open(path, 'r') as file:
         return file.read()
-    
+
+
 def compile_shader(source, shader_type):
     shader = glCreateShader(shader_type)
-    
+
     glShaderSource(shader, source)
     glCompileShader(shader)
-    
+
     sucess = glGetShaderiv(shader, GL_COMPILE_STATUS)
-    
+
     if not sucess:
         error = glGetShaderInfoLog(shader).decode()
-        raise RuntimeError(f"{Fore.RED}Shader compile error: {error}{Style.RESET_ALL}")
-    
+        raise RuntimeError(f"{Fore.RED}Shader compile error: {
+                           error}{Style.RESET_ALL}")
+
     return shader
+
+
+def check_shader_compile_status(shader, name):
+    status = glGetShaderiv(shader, GL_COMPILE_STATUS)
+
+    if not status:
+        log = glGetShaderInfoLog(shader).decode()
+
+        raise RuntimeError(f"{name} compilation error: {log}")
+
+
+def check_program_link_status(program):
+    status = glGetProgramiv(program, GL_LINK_STATUS)
+    if not status:
+        log = glGetProgramInfoLog(program).decode()
+        raise RuntimeError(f"Program linking error: {log}")
