29767e47f46a8e04a595e0bb0055e69033016c50
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2024-11-26 02:51:44 -0600

    update: entrypoint
    Ask for resolution, for changes:
    
    give users the option to select from a list of choices.

--- __main__.py
+++ __main__.py
@@ -1,5 +1,11 @@
 from src.Application import Application
+from colorama import init, Fore, Style
 
 if __name__ == "__main__":
-    app = Application()
+    init()
+    
+    width = int(input(f"{Fore.CYAN}Desired width of the window: {Style.RESET_ALL}"))
+    height = int(input(f"{Fore.CYAN}Desired height of the window: {Style.RESET_ALL}"))
+    
+    app = Application(width, height)
     app.run() 
